283219138b3f13dd3a1d6c5fce66b728c910cf08
Sulochana Kodituwakku <sulochana.456@live.com>
2017-02-26 18:07:44 +0530

    minor testings

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ def tryread(obj):
     r = obj.read(1)
 
     if r != "":
-        return ord(r)
+        return 255-ord(r)
     else:
         global go
         go = False
         print "err"
-        return 0
+        return 255-0
 
 
 
@@ -21,7 +21,7 @@ file = "file.mp3"
 f = open(file, "rb")
 l1=[]
 l2=[]
-fs = os.path.getsize(file)/3
+fs = os.path.getsize(file)/4
 print "File size",fs,"Bytes"
 w = int(math.sqrt(fs))
 im = Image.new("RGBA", (w+1, w+1), "white")
@@ -32,10 +32,17 @@ i=0
 
 try:
     while go:
-        r,g,b,a = tryread(f),tryread(f),tryread(f),255
-        # print i
+        r,g,b,a = tryread(f),tryread(f),tryread(f),tryread(f)
         im.putpixel((i//(w+1),i%(w+1)),(r,g,b,a))
         i+=1
 finally:
     im.save("out.png")
     f.close()
+
+
+'''
+1 1 0
+0 1 1
+1 0 1
+0 0 0
+'''
